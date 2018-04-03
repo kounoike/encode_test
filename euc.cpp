d167219f1c37ef7aca7ac02bc07350b124a7b602
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-03 22:55:39 +0900

    add sjis/euc.cpp

---
+++ euc.cpp
@@ -0,0 +1,987 @@
+#include <stdio.h>
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+int f(int a, int b, int c){
+    int sum = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < c; k++){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
+}
+
+// あいうえおかきくけこ
